2c933b3adc60a111324b4a92a740ed336f55212e
Mark Mayer <mdm373@gmail.com>
2020-05-03 20:54:08 -0400

    initial color styling

--- packages/rundot-theme/src/custom-header.tsx
+++ packages/rundot-theme/src/custom-header.tsx
@@ -1,3 +1,3 @@
 import React from "react"
 
-export const CustomHeader: () => JSX.Element = () => (<div className="header">hello world</div>)
+export const CustomHeader: () => JSX.Element = () => (<div></div>)
